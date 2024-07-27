b60840ccfbf4966189887000ffb5c2dfdcc285d0
Davinderpal Singh Rehal <davinderpalrehal@gmail.com>
2024-07-27 16:16:59 +0300

    Adding login flag for user type

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -12,13 +12,16 @@ class NumuwUser(AbstractUser):
 
 class Patient(models.Model):
     user = models.OneToOneField(NumuwUser, on_delete=models.CASCADE)
+    can_login = models.BooleanField(default=False)
 
 
 class Parent(models.Model):
     user = models.OneToOneField(NumuwUser, on_delete=models.CASCADE)
     patients = models.ManyToManyField(Patient,  related_name='parents')
+    can_login = models.BooleanField(default=True)
 
 
 class Therapist(models.Model):
     user = models.OneToOneField(NumuwUser, on_delete=models.CASCADE)
     patients = models.ManyToManyField(Patient, related_name='therapists')
+    can_login = models.BooleanField(default=True)
--- backend/numuw_chat/urls.py
+++ backend/numuw_chat/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, status
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from accounts.models import NumuwUser
 from .views import ConversationViewSet, MessageViewSet
 from accounts.views import UserDetailView
 
@@ -10,8 +13,27 @@ router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 router.register(r'user_details', UserDetailView, basename='user-details')
 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+
+        try:
+            user = NumuwUser.objects.get(username=username)
+            if not user.can_login:
+                return Response({
+                    'error': 'User is not allowed to login'
+                },
+                status=status.HTTP_403_FORBIDDEN)
+        except NumuwUser.DoesNotExist:
+            return Response({
+                'error': 'User does not exist'
+            },
+            status=status.HTTP_404_NOT_FOUND)
+
+        return super().post(request, *args, **kwargs)
+
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
